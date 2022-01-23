5feedae861773048b9efb20bf12947e0baf8461d
ParkSangSun <appleasz1028102@gmail.com>
2022-01-23 13:56:56 +0900

    Create : MainFragment

---
+++ presentation/src/main/java/com/pss/presentation/view/MainFragment.kt
@@ -0,0 +1,18 @@
+package com.pss.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.presentation.R
+import com.pss.presentation.base.BaseFragment
+import com.pss.presentation.databinding.FragmentMainBinding
+
+
+class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
+
+    override fun init() {
+
+    }
+}
