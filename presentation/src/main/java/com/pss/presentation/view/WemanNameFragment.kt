59405bf437b0ca5af9d9488b7a2440708f1f603d
ParkSangSun <appleasz1028102@gmail.com>
2022-01-25 22:47:11 +0900

    Create : 여자 이름 작성

---
+++ presentation/src/main/java/com/pss/presentation/view/WemanNameFragment.kt
@@ -0,0 +1,17 @@
+package com.pss.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.presentation.R
+import com.pss.presentation.base.BaseFragment
+import com.pss.presentation.databinding.FragmentWemanNameBinding
+
+class WemanNameFragment : BaseFragment<FragmentWemanNameBinding>(R.layout.fragment_weman_name) {
+
+    override fun init() {
+
+    }
+}
