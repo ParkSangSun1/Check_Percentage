229eb66b0d203ae4cf4939c99e05ced165553287
ParkSangSun <appleasz1028102@gmail.com>
2022-01-25 22:46:58 +0900

    Create : 남자 이름 작성 xml

---
+++ presentation/src/main/java/com/pss/presentation/view/ManNameFragment.kt
@@ -0,0 +1,17 @@
+package com.pss.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.presentation.R
+import com.pss.presentation.base.BaseFragment
+import com.pss.presentation.databinding.FragmentManNameBinding
+
+class ManNameFragment : BaseFragment<FragmentManNameBinding>(R.layout.fragment_man_name) {
+
+    override fun init() {
+
+    }
+}
