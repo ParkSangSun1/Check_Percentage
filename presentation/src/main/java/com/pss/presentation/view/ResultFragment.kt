a0f8fda87ea07aac070c6a5b49baa36e065dc212
ParkSangSun <appleasz1028102@gmail.com>
2022-01-25 23:05:56 +0900

    Create : ResultFragment.kt

---
+++ presentation/src/main/java/com/pss/presentation/view/ResultFragment.kt
@@ -0,0 +1,17 @@
+package com.pss.presentation.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.pss.presentation.R
+import com.pss.presentation.base.BaseFragment
+import com.pss.presentation.databinding.FragmentResultBinding
+
+class ResultFragment : BaseFragment<FragmentResultBinding>(R.layout.fragment_result) {
+
+    override fun init() {
+
+    }
+}
