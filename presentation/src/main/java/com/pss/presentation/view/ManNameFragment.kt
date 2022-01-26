f34992709a4f0d465810c0877cd95e3acbf266ce
ParkSangSun <appleasz1028102@gmail.com>
2022-01-26 20:30:28 +0900

    Add : 남자 이름 입력 화면 다음버튼

--- presentation/src/main/java/com/pss/presentation/view/ManNameFragment.kt
+++ presentation/src/main/java/com/pss/presentation/view/ManNameFragment.kt
@@ -1,10 +1,12 @@
 package com.pss.presentation.view
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.pss.presentation.R
 import com.pss.presentation.base.BaseFragment
 import com.pss.presentation.databinding.FragmentManNameBinding
@@ -12,6 +14,11 @@ import com.pss.presentation.databinding.FragmentManNameBinding
 class ManNameFragment : BaseFragment<FragmentManNameBinding>(R.layout.fragment_man_name) {
 
     override fun init() {
+        binding.fragment = this
+    }
 
+    fun nextBtnClick(view: View){
+        if (TextUtils.isEmpty(binding.editTxt.text.toString())) shortShowToast("이름을 입력해 주세요")
+        else this.findNavController().navigate(R.id.action_manNameFragment_to_resultFragment)
     }
 }
