c4c4164a246397c93fb3de05d0fcc1f4a22deb61
ParkSangSun <appleasz1028102@gmail.com>
2022-01-26 20:18:15 +0900

    Add : 확인하기 버튼 클릭

--- presentation/src/main/java/com/pss/presentation/view/MainFragment.kt
+++ presentation/src/main/java/com/pss/presentation/view/MainFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.pss.presentation.R
 import com.pss.presentation.base.BaseFragment
 import com.pss.presentation.databinding.FragmentMainBinding
@@ -13,6 +14,10 @@ import com.pss.presentation.databinding.FragmentMainBinding
 class MainFragment : BaseFragment<FragmentMainBinding>(R.layout.fragment_main) {
 
     override fun init() {
+        binding.fragment = this
+    }
 
+    fun startBtnClick(view: View){
+        this.findNavController().navigate(R.id.action_mainFragment_to_wemanNameFragment)
     }
 }
